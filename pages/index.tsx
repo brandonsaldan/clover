f50470101bd995e2209082aee0dfc492c6f22137
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2022-08-04 23:14:48 -0400

    feat: add head tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,16 @@ const Home: NextPage = () => {
   return (
     <div className="bg-white dark:bg-[#000000] flex min-h-screen flex-col py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Brandon Saldan - Website and Portfolio</title>
+        <meta name="description" content="Business and Computer Science student from Pennsylvania" />
+        <meta name="keywords" content="Brandon Saldan, Saldan, brandons.place, saldan.dev" />
+        <meta name="author" content="Brandon Saldan" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://brandons.place/" />
+        <meta property="og:title" content="Brandon Saldan - Website and Portfolio" />
+        <meta property="og:description" content="Business and Computer Science student from Pennsylvania" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
         <script src="https://kit.fontawesome.com/d6ce3288bb.js" crossorigin="anonymous"></script>
       </Head>
