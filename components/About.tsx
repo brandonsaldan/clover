a14ecd6b58c4414755afdde848c97cbe7a26c22b
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2022-08-06 15:41:32 -0400

    fix: add hrefs

--- components/About.tsx
+++ components/About.tsx
@@ -5,9 +5,9 @@ const About = () => {
         <div className="text-black dark:text-white px-6 sm:px-0">
             <Text h3>Brandon Saldan</Text>
             <Text p className="mt-1">Hello! I'm a freshman in college majoring in business administration with plans to finish my bachelor's degree in management information systems and (hopefully) complete an MBA.</Text>
-            <Text p className="mt-1">I have five years of experience in enterprise software engineering and previously worked as a Business Analyst for the <a className="underline ru text-black dark:text-white">City of Raleigh Municipal Government</a>.</Text>
-            <Text p className="mt-1">I'm currently working at <a className="underline iu text-black dark:text-white">Invii</a>. We're building an all-in-one restaurant management solution, come check us out!</Text>
-            <Text p className="mt-1">Feel free to contact me with any questions by connecting on <a className="underline lu text-black dark:text-white">LinkedIn</a> or messaging me on <a className="underline tu text-black dark:text-white">Twitter</a>!</Text>
+            <Text p className="mt-1">I have five years of experience in enterprise software engineering and previously worked as a Business Analyst for the <a className="underline ru text-black dark:text-white" href="https://raleighnc.gov/entrepreneurship-and-innovation/">City of Raleigh Municipal Government</a>.</Text>
+            <Text p className="mt-1">I'm currently working at <a className="underline iu text-black dark:text-white" href="https://invii.io/">Invii</a>. We're building an all-in-one restaurant management solution, come check us out!</Text>
+            <Text p className="mt-1">Feel free to contact me with any questions by connecting on <a className="underline lu text-black dark:text-white" href="https://www.linkedin.com/in/brandonsaldan/">LinkedIn</a> or messaging me on <a className="underline tu text-black dark:text-white" href="https://twitter.com/brandonsaldan/">Twitter</a>!</Text>
         </div>
     );
 }
