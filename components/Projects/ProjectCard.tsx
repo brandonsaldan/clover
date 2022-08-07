3498288a9a9208aaa549e4103b6603ea89534833
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2022-08-06 20:02:10 -0400

    fix: link to repos

--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -7,21 +7,25 @@ const projects = [
       name: 'Sassafras',
       desc: 'A minimalist portfolio template built with Vue and Tailwind.',
       lang: 'Vue',
+      href: 'https://github.com/brandonsaldan/sassafras'
     },
     {
       name: 'Fox',
       desc: 'A social media username scraping Discord bot built with Python.',
       lang: 'Python',
+      href: 'https://github.com/brandonsaldan/fox'
     },
     {
       name: 'Clover',
       desc: 'Personal website built with NextJS, Tailwind, and Geist.',
       lang: 'TypeScript',
+      href: 'https://github.com/brandonsaldan/clover'
     },
     {
       name: 'Atlas',
       desc: 'Personal website template built with Vue and Tailwind.',
       lang: 'Vue',
+      href: 'https://github.com/brandonsaldan/atlas'
     },
 ]
 
@@ -29,13 +33,15 @@ const About = () => {
     return (
         <ul role="list" className="mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 px-6 sm:px-0">
             {projects.map((project) => (
+              <a href={project.href}>
                 <div className="fieldset-270 bg-white">
                     <div className="fieldset-352 bg-white dark:bg-black">
                       <div className="fieldset-410 text-black dark:text-white">{project.name}</div>
                       <div className="fieldset-226 w-72 sm:w-80 text-black dark:text-white">{project.desc}</div>
                     </div>
                     <footer className="fieldset-174 bg-[#fafafa] dark:bg-[#101010] dark:text-white">{project.lang}</footer>
-                </div>            
+                </div> 
+                </a>           
             ))}
         </ul>
     );
