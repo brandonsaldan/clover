488d5d458a856dc75790553b11ff34260185d6a2
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2022-10-05 15:49:31 -0400

    feat: add animate.css

--- components/Status.tsx
+++ components/Status.tsx
@@ -1,8 +1,9 @@
 import { useLanyardWs } from "use-lanyard";
 import { SiSpotify, SiVisualstudiocode } from "react-icons/si";
+import 'animate.css';
 
 const Status = () => {
-    const data = useLanyardWs("");
+    const data = useLanyardWs("261953350860275713");
     const listening = data?.spotify || null;
     const coding = data?.activities.find(
         (a) => a.application_id === "383226320970055681"
@@ -10,19 +11,23 @@ const Status = () => {
 
     if (coding) {
         return (
-            <div className="flex w-full bg-black dark:bg-[#1ea1dc] sm:rounded-lg items-center text-white leading-none h-8 ">
-            <SiVisualstudiocode className="ml-4 h-4 w-4" />
-                <p className="ml-2 truncate">Editing<a className="font-semibold text-inherit">{coding.details?.replace("Editing", "")}</a> in <a className="font-semibold text-inherit">{coding.name}</a></p>
+            <div className="animate__animated animate__fadeInUp">
+                <div className="flex w-full bg-black dark:bg-[#1ea1dc] sm:rounded-lg items-center text-white leading-none h-8 ">
+                <SiVisualstudiocode className="ml-4 h-4 w-4" />
+                    <p className="ml-2 truncate">Editing<a className="font-semibold text-inherit">{coding.details?.replace("Editing", "")}</a> in <a className="font-semibold text-inherit">{coding.name}</a></p>
+                </div>
             </div>
         )
     } else if (listening) {
         return (
-            <a href={`https://open.spotify.com/track/${listening.track_id}`}>
-            <div className="flex w-full bg-black dark:bg-[#1DB954] sm:rounded-lg items-center text-white leading-none h-8 ">
-            <SiSpotify className="ml-4 h-4 w-4" />
-                <p className="ml-2 truncate">Listening to <a className="font-semibold text-inherit">{listening.song}</a> by <a className="font-semibold text-inherit">{listening.artist}</a></p>
+            <div className="animate__animated animate__fadeInUp">
+                <a href={`https://open.spotify.com/track/${listening.track_id}`}>
+                <div className="flex w-full bg-black dark:bg-[#1DB954] sm:rounded-lg items-center text-white leading-none h-8 ">
+                <SiSpotify className="ml-4 h-4 w-4" />
+                    <p className="ml-2 truncate">Listening to <a className="font-semibold text-inherit">{listening.song}</a> by <a className="font-semibold text-inherit">{listening.artist}</a></p>
+                </div>
+                </a>
             </div>
-            </a>
         )
     } return (
         <div className="hidden">
