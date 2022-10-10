9b1b6049a4d09188f884dd003732d32c1095876c
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2022-10-09 22:14:22 -0400

    feat: get pinned repos from github

--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -1,50 +1,57 @@
 import useSound from "use-sound";
+import { useEffect, useState } from "react";
+
+import { TbGitFork } from "react-icons/tb"
+import { AiFillStar } from "react-icons/ai"
 
 const hoverSound = "/sound/hover.mp3";
 
-const projects = [
-    {
-      name: 'Sassafras',
-      desc: 'A minimalist portfolio template built with Vue and Tailwind.',
-      lang: 'Vue',
-      href: 'https://github.com/brandonsaldan/sassafras'
-    },
-    {
-      name: 'Fox',
-      desc: 'A social media username scraping Discord bot built with Python.',
-      lang: 'Python',
-      href: 'https://github.com/brandonsaldan/fox'
-    },
-    {
-      name: 'Clover',
-      desc: 'Personal website built with NextJS, Tailwind, and Geist.',
-      lang: 'TypeScript',
-      href: 'https://github.com/brandonsaldan/clover'
-    },
-    {
-      name: 'Atlas',
-      desc: 'Personal website template built with Vue and Tailwind.',
-      lang: 'Vue',
-      href: 'https://github.com/brandonsaldan/atlas'
-    },
-]
-
 const About = () => {
     const [play] = useSound(hoverSound);
+    const [projects, setProjects] = useState<any[]>([]);
+    const getPinnedRepos = async () => {
+      fetch("https://gh-pinned-repos.egoist.dev/?username=brandonsaldan")
+      .then((response) => response.json())
+      .then((data) => setProjects(data))
+      .catch((err) => console.log(err));
+      };
+
+    useEffect(() => {
+      getPinnedRepos();
+    }, []);
     return (
-        <ul role="list" className="mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 px-6 sm:px-0">
-            {projects.map((project) => (
-              <a href={project.href}>
-                <div onMouseEnter={() => play()} className="fieldset-270 bg-white transition ease-in-out delay-50 hover:scale-105">
-                    <div className="fieldset-352 bg-white dark:bg-[#111111]">
-                      <div className="fieldset-410 text-black dark:text-white">{project.name}</div>
-                      <div className="fieldset-226 w-72 sm:w-80 text-black dark:text-white">{project.desc}</div>
+      <ul role="list" className="mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 px-6 sm:px-0">
+          {projects.map((project, key) => {
+            return (
+              <a href={project.link}>
+              <div key={key} onMouseEnter={() => play()} className="fieldset-270 bg-white transition ease-in-out delay-50 hover:scale-105">
+                  <div className="fieldset-352 bg-white dark:bg-[#111111]">
+                    <div className="fieldset-410 text-black dark:text-white">{project.repo.charAt(0).toUpperCase() + project.repo.slice(1)}</div>
+                    <div className="fieldset-226 w-72 sm:w-80 text-black dark:text-white">{project.description}</div>
+                  </div>
+                  <footer className="fieldset-174 bg-[#fafafa] dark:bg-[#141414] dark:text-white">
+                    <div className="text-black dark:text-white">{project.language}</div>
+                    <div className="mt-1 flex">
+                      <div className="flex pr-3">
+                        <div className="mt-1 h-5 w-5 text-black dark:text-white">
+                          <AiFillStar />
+                        </div>
+                        {project.stars}
+                      </div>
+
+                      <div className="flex">
+                        <div className="mt-1 h-5 w-5 text-black dark:text-white">
+                          <TbGitFork />
+                        </div>
+                        {project.forks}
+                      </div>
                     </div>
-                    <footer className="fieldset-174 bg-[#fafafa] dark:bg-[#141414] dark:text-white">{project.lang}</footer>
-                </div> 
-                </a>    
-            ))}
-        </ul>
+                  </footer>
+              </div> 
+              </a> 
+            )   
+          })}
+      </ul>
     );
 }
 
