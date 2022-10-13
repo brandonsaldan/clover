ec8bf8db9600209f2354c9d40f343fa710adccb6
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2022-10-12 21:48:02 -0400

    fix: change status from black on light mode

--- components/Status.tsx
+++ components/Status.tsx
@@ -12,7 +12,7 @@ const Status = () => {
     if (coding) {
         return (
             <div className="animate__animated animate__fadeInUp">
-                <div className="flex w-full bg-black dark:bg-[#1ea1dc] sm:rounded-lg items-center text-white leading-none h-8 ">
+                <div className="flex w-full bg-[#1ea1dc] sm:rounded-lg items-center text-white leading-none h-8 ">
                 <SiVisualstudiocode className="ml-4 h-4 w-4" />
                     <p className="ml-2 truncate">Editing<a className="font-semibold text-inherit">{coding.details?.replace("Editing", "")}</a> in <a className="font-semibold text-inherit">{coding.name}</a></p>
                 </div>
@@ -22,7 +22,7 @@ const Status = () => {
         return (
             <div className="animate__animated animate__fadeInUp">
                 <a href={`https://open.spotify.com/track/${listening.track_id}`}>
-                <div className="flex w-full bg-black dark:bg-[#1DB954] sm:rounded-lg items-center text-white leading-none h-8 ">
+                <div className="flex w-full bg-[#1DB954] sm:rounded-lg items-center text-white leading-none h-8 ">
                 <SiSpotify className="ml-4 h-4 w-4" />
                     <p className="ml-2 truncate">Listening to <a className="font-semibold text-inherit">{listening.song}</a> by <a className="font-semibold text-inherit">{listening.artist}</a></p>
                 </div>
